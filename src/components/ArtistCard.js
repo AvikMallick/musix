d344f4575347766afe1352c782139637237ea575
Avik Mallick <avikmallick111101@gmail.com>
2023-07-11 13:52:42 +0530

    added null check for artist name in artist card

--- src/components/ArtistCard.js
+++ src/components/ArtistCard.js
@@ -1,27 +1,27 @@
 import { Card, CardContent, CardMedia, Typography } from '@mui/material';
 
 const ArtistCard = ({ artist }) => {
-	console.log(artist);
-	return (
-		<Card
-			sx={{
-				maxWidth: '10rem',
-				height: '12rem',
-				flexGrow: '1',
-			}}
-		>
-			<CardMedia
-				sx={{ height: '9rem', width: 'auto' }}
-				image={artist?.avatar}
-				style={{
-					mask: 'linear- gradient(to right, rgba(0, 0, 0, 1.0) 50%, transparent 100%)',
-				}}
-			></CardMedia>
-			<CardContent sx={{ height: '0.5rem' }}>
-				<Typography>{artist.name}</Typography>
-			</CardContent>
-		</Card>
-	);
+  console.log(artist);
+  return (
+    <Card
+      sx={{
+        maxWidth: '10rem',
+        height: '12rem',
+        flexGrow: '1',
+      }}
+    >
+      <CardMedia
+        sx={{ height: '9rem', width: 'auto' }}
+        image={artist?.avatar}
+        style={{
+          mask: 'linear- gradient(to right, rgba(0, 0, 0, 1.0) 50%, transparent 100%)',
+        }}
+      ></CardMedia>
+      <CardContent sx={{ height: '0.5rem' }}>
+        <Typography>{artist?.name}</Typography>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default ArtistCard;
